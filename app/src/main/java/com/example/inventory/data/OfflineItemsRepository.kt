081e5a34d890aed7c4f3ec2b9e4360679efb4d3c
Salih Akbas <salihhakbass@gmail.com>
2024-08-05 11:39:32 +0300

    ItemsRepository and ItemDao created

--- app/src/main/java/com/example/inventory/data/OfflineItemsRepository.kt
+++ app/src/main/java/com/example/inventory/data/OfflineItemsRepository.kt
@@ -16,4 +16,26 @@
 
 package com.example.inventory.data
 
-class OfflineItemsRepository : ItemsRepository
+import kotlinx.coroutines.flow.Flow
+
+class OfflineItemsRepository(private val itemDao: ItemDao) : ItemsRepository {
+    override fun getAllItemsStream(): Flow<List<Item>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemStream(id: Int): Flow<Item?> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertItem(item: Item) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteItem(item: Item) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateItem(item: Item) {
+        TODO("Not yet implemented")
+    }
+}
